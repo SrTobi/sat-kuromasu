a67b5428b16746d43fa33a3f90f9e7fa1ddbf13d
SrTobi <code.databyte@gmail.com>
2016-12-22 15:14:05 +0100

    added numbers

--- MyKuromasuSolver.java
+++ MyKuromasuSolver.java
@@ -91,9 +91,14 @@ public class MyKuromasuSolver extends KuromasuSolver {
 	}
 
 	class Number {
-		int[] bits;
+		private int max;
+		protected int[] bits;
 
-		Number(int numBits) {
+		Number(int max, int numBits) {
+			if(max >= (int)Math.pow(2, numBits)) {
+				throw new IllegalArgumentException("max to big");
+			}
+			this.max = max;
 			this.bits = new int[numBits];
 
 			for(int i = 0; i < bits.length; ++i) {
@@ -101,25 +106,129 @@ public class MyKuromasuSolver extends KuromasuSolver {
 			}
 		}
 
-		public int bits() {
+		Number(int max) {
+			this(max, (int)Math.floor(Math.log(max) / Math.log(2)) + 1);
+		}
+
+		int bits() {
 			return bits.length;
 		}
 
-		public int bit(int i) {
+		int bit(int i) {
 			return bits[i];
 		}
 
-		public Number add(Number other) {
-			Number result = new Number(Math.max(bits(), other.bits()) + 1);
-			Number smaller = this;
-			Number bigger = other;
-			if(smaller.bits() > bigger.bits()) {
+		int max() {
+			return max;
+		}
 
+		public Number add(Number other) {
+			Number result = new Number(max() + other.max());
+			Number smaller = bits() < other.bits()? this : other;
+			Number bigger = bits() >= other.bits()? this : other;
+
+			int carryIn = falseVar;
+			for(int i = 0; i < smaller.bits(); ++i) {
+				int big = bigger.bit(i);
+				int small = smaller.bit(i);
+				int res = result.bit(i);
+				int carryOut = newVar();
+
+				int[][] bothIn = allOf(big, small);
+
+
+				makeClausesForFullBitAdder(small, big, carryIn, carryOut, res);
+
+				carryIn = carryOut;
 			}
 
+			for(int i = smaller.bits(); i < bigger.bits(); ++i) {
+				int big = bigger.bit(i);
+				int res = result.bit(i);
+				int carryOut = newVar();
 
+				// newCarry <=> big && carryIn
+				addClause( carryOut, -carryIn, -big);
+				addClause(-carryOut, carryIn);
+				addClause(-carryOut, big);
+
+				// res <=> big XOR carryIn
+				addClause(-res, -carryIn, -big);
+				addClause(-res, carryIn, big);
+				addClause(res, -carryIn, big);
+				addClause(res, carryIn,-big);
+
+				carryIn = carryOut;
+			}
+
+			boolean extraBit = result.bits() > bigger.bits();
+			addTerm(allOrNone(carryIn, extraBit? result.bit(result.bits() - 1) : falseVar));
 
 			return result;
+		}
+
+		public int[][] equalTo(Number other) {
+			Number smaller = bits() < other.bits()? this : other;
+			Number bigger = bits() >= other.bits()? this : other;
+			int[][][] result = new int[bigger.bits()][][];
+			for(int i = 0; i < bigger.bits(); ++i) {
+				int small = i < smaller.bits()? smaller.bit(i) : falseVar;
+				int big = bigger.bit(i);
+				result[i] = allOrNone(small, big);
+			}
+			return allOf(result);
+		}
+
+		private void makeClausesForFullBitAdder(int x, int y, int carryIn, int carryOut, int res) {
+			// Clauses for fullbitadder's output
+			// (carryIn && (x XOR y) || (x && y)) <=> carryOut
+			addClause(-carryIn, carryOut, -x);
+			addClause(-carryIn, carryOut, -y);
+			addClause(carryIn, -carryOut, x);
+			addClause(carryIn, -carryOut, y);
+			addClause(-carryOut, x, y);
+			addClause(carryOut, -x, -y);
+
+			// carryIn XOR x XOR y <=> res
+			addClause(-res, -x, -y, carryIn);
+			addClause(-res, -x, y, -carryIn);
+			addClause(-res, x, -y, -carryIn);
+			addClause(-res, x, y, carryIn);
+			addClause(res, -x, -y, -carryIn);
+			addClause(res, -x, y, carryIn);
+			addClause(res, x, -y, carryIn);
+			addClause(res, x, y, -carryIn);
+		}
+
+		public String toString(ISolver solver) {
+			StringBuilder bld = new StringBuilder();
+			bld.append("[");
+			int num = 0;
+			int setter = 1 << bits() - 1;
+			for(int i = bits() - 1; i >= 0; --i) {
+				boolean isSet = solver.model(bit(i));
+				bld.append(isSet? 1 : 0);
+				num |= isSet? setter : 0;
+				setter >>= 1;
+			}
+
+			bld.append("](");
+			bld.append(num);
+			bld.append(")");
+
+			return bld.toString();
+		}
+	}
+
+	class Constant extends Number {
+
+		Constant(int c) {
+			super(c);
+
+			for(int i = 0; i < bits(); ++i) {
+				int tester = 1 << i;
+				bits[i] = ((c & tester) == 0)? falseVar : trueVar;
+			}
 		}
 	}
 
@@ -140,6 +249,8 @@ public class MyKuromasuSolver extends KuromasuSolver {
 	private int height;
 	private int width;
 	private int nextVar;
+	private int falseVar;
+	private int trueVar;
 
 	public MyKuromasuSolver(Kuromasu k) {
 		super(k);
@@ -147,6 +258,13 @@ public class MyKuromasuSolver extends KuromasuSolver {
 		this.height = k.getHeight();
 		this.numFields = k.getHeight() * k.getWidth();
 		this.nextVar = numFields + 1;
+
+		// register static vars
+		falseVar = newVar();
+		addClause(-falseVar);
+
+		trueVar = newVar();
+		addClause(trueVar);
 	}
 
 	@Override
@@ -186,7 +304,7 @@ public class MyKuromasuSolver extends KuromasuSolver {
 
 		// Visualize the Solution
 		//solution.show();
-		// or on the terminal
+		//, on the terminal
 		solution.print();
 
 		return solution;
@@ -399,6 +517,16 @@ public class MyKuromasuSolver extends KuromasuSolver {
 		return allOf(conds);
 	}
 
+	private int[][] exactlyOneOf(int[][]... args) {
+		int[][][] conds = new int[args.length + 1][][];
+		for(int i = 0; i < args.length; ++i) {
+			int[][][] rest = rest(i, args);
+			conds[i] = when(args[i]).then(noneOf(rest));
+		}
+		conds[conds.length - 1] = atLeastOneOf(args);
+		return allOf(conds);
+	}
+
 	private int[][] allOrNone(int... args) {
 		int[][][] conds = new int[args.length][][];
 		for(int i = 0; i < args.length; ++i) {
@@ -418,14 +546,27 @@ public class MyKuromasuSolver extends KuromasuSolver {
 		return rest;
 	}
 
+	private int[][][] rest(int i, int[][][] list) {
+		int[][][] rest = new int[list.length - 1][][];
+		for(int j = 0; j < list.length; ++j) {
+			if (i != j) {
+				rest[j < i ? j : j - 1] = list[j];
+			}
+		}
+		return rest;
+	}
+
 	private void test() throws ContradictionException {
 		//int[][] term = noneOf(allOf(atLeastOneOf(1, 2), atLeastOneOf(3, 4)), allOf(atLeastOneOf(5, 6), atLeastOneOf(7, 8)));
-		int[][] term = allOrNone(1,2,3,4);
-		addTerm(term);
-
+		Number n = new Number(3);
+		Number n2 = new Number(3);
+		Number n3 = new Constant(5);
+		//n2.add(n);
+		int[][] term = n.add(n2).equalTo(n3);
 
 		for(int[] t : term) {
-			solver.addClause(new VecInt(t));
+			if(t.length > 0)
+				solver.addClause(new VecInt(t));
 		}
 
 		try {
@@ -438,6 +579,10 @@ public class MyKuromasuSolver extends KuromasuSolver {
 				for (int m : model) {
 					System.out.println(Math.abs(m) + " -> " + (m > 0));
 				}
+
+				System.out.println("n = " + n.toString(mi));
+				System.out.println("n2 = " + n2.toString(mi));
+				System.out.println("n3 = " + n3.toString(mi));
 			}
 			if(unsat) {
 				System.out.println("Not satisfiable");
